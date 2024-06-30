39feadc941570cdb42a57334bcb86826738d3995
chsinnnn <102168453+chsinnnn@users.noreply.github.com>
2024-06-30 14:53:10 +0800

    6/30Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from linebot.models import QuickReply, QuickReplyButton, MessageAction, FlexSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, QuickReply, QuickReplyButton, MessageAction, FlexSendMessage
 from pymongo import MongoClient
 import random
 import json
 import redis
 import requests
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -25,6 +25,7 @@ handler = WebhookHandler(secret)
 mongo_client = MongoClient("mongodb://localhost:27017/")
 mongo_db = mongo_client["testdb"]
 mongo_collection = mongo_db["user_data"]
+suggestion_collection = mongo_db["suggestions"]
 
 # 連Redis
 redis_host = 'localhost'
@@ -47,31 +48,19 @@ def is_valid_student_id(student_id):
 
 def handle_student_id(user_id, user_name, msg):
     if is_valid_student_id(msg):
-        # 學號格式正確，儲存到Redis和MongoDB
-        if not redis_client.hexists(user_id, 'student_id'):
-            redis_client.hset(user_id, mapping={'name': user_name, 'student_id': msg})
-            mongo_collection.insert_one({"user_id": user_id, "name": user_name, "student_id": msg})
-            reply = f"學號已紀錄成功！{user_name}"
-        else:
+        # 學號格式正確，檢查學號是否已經存在於 MongoDB 中
+        existing_user = mongo_collection.find_one({"user_id": user_id})
+        if existing_user:
             reply = f"{user_name}，您的學號已經登錄過了。"
+        else:
+            # 將學號存入 Redis 中
+            redis_client.hset(user_id, 'student_id', msg)
+            # 同時將學號存入 MongoDB 中
+            student_data = {"user_id": user_id, "name": user_name, "student_id": msg}
+            mongo_collection.insert_one(student_data)
+            reply = f"{user_name}，學號已紀錄成功！請點選表單的我要作答並選題目!!"
     else:
         reply = "學號格式不正確，請輸入8位數字的學號。"
-    return reply
-
-def handle_question_reply(user_id, user_name, msg):
-    if redis_client.hexists(user_id, 'student_id'):
-        student_id = redis_client.hget(user_id, 'student_id')
-        redis_client.rpush(f"{student_id}_questions", msg)
-        redis_client.expire(f"{student_id}_questions", 600)
-        reply = f"登入成功！{user_name}，您的學號是 {student_id}，您問的問題是：{msg}。我們會在10分鐘內刪除這條記錄。"
-    elif mongo_collection.find_one({"user_id": user_id}):
-        user_record = mongo_collection.find_one({"user_id": user_id})
-        student_id = user_record['student_id']
-        redis_client.rpush(f"{student_id}_questions", msg)
-        redis_client.expire(f"{student_id}_questions", 600)
-        reply = f"登入成功！{user_name}，您的學號是 {student_id}，您問的問題是：{msg}。我們會在10分鐘內刪除這條記錄。"
-    else:
-        reply = f"{user_name}，請問您的學號是多少？"
     return reply
 
 def handle_unit_selection(event):
@@ -87,7 +76,7 @@ def handle_unit_selection(event):
     message = TextSendMessage(text="請選擇一個單元", quick_reply=quick_reply)
     line_bot_api.reply_message(event.reply_token, message)
 
-def handle_question_display(event, unit):
+def handle_question_display(event, unit):  # 多個題目挑選
     collection = unit_collections[unit]
 
     questions = list(collection.find())
@@ -108,7 +97,8 @@ def handle_question_display(event, unit):
                         "size": "xl",
                         "wrap": True,
                         "align": "center",
-                        "gravity": "center"
+                        "gravity": "center",
+                        "color": "#FFFFFF"
                     }
                 ]
             },
@@ -120,7 +110,7 @@ def handle_question_display(event, unit):
                         "type": "text",
                         "text": question["Question"],
                         "wrap": True,
-                        "size": "lg"
+                        "size": "md"
                     }
                 ]
             },
@@ -139,6 +129,11 @@ def handle_question_display(event, unit):
                         }
                     }
                 ]
+            },
+            "styles": {
+                "header": {
+                    "backgroundColor": "#668166" #header底色
+                }
             }
         }
         bubbles.append(bubble)
@@ -152,7 +147,7 @@ def handle_question_display(event, unit):
     )
     line_bot_api.reply_message(event.reply_token, flex_message)
 
-def handle_question_answer(event, question_title):
+def handle_question_answer(event, question_title):  # 已選好題目
     question = None
     for unit, collection in unit_collections.items():
         question = collection.find_one({"Question": question_title})
@@ -160,63 +155,66 @@ def handle_question_answer(event, question_title):
             break
 
     if question:
-        # 這裡新增了將問題丟給 Llama3 伺服器並回傳答案的部分
-        llama3_answer = send_question_to_llama3(question["Question"])
-        flex_message = FlexSendMessage(
-            alt_text="題目詳情",
-            contents={
-                "type": "bubble",
-                "header": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": "題目",
-                            "weight": "bold",
-                            "size": "xl",
-                            "wrap": True,
-                            "align": "center",
-                            "gravity": "center"
-                        }
-                    ]
-                },
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                        {
-                            "type": "text",
-                            "text": question["Question"],
-                            "wrap": True,
-                            "size": "lg"
-                        },
-                        {
-                            "type": "text",
-                            "text": f"Llama3 回答: {llama3_answer}",
-                            "wrap": True,
-                            "size": "lg"
-                        },
-                        {
-                            "type": "button",
-                            "style": "primary",
-                            "color": "#EBA281",
-                            "action": {
-                                "type": "message",
-                                "label": "開始作答",
-                                "text": f"開始作答{question['Question']}"
-                            }
-                        }
-                    ]
-                }
-            }
-        )
-        line_bot_api.reply_message(event.reply_token, flex_message)
+        user_id = event.source.user_id
+        redis_client.hset(user_id, "current_question", question_title)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"請回答以下問題：\n\n{question_title}"))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="找不到題目"))
 
+def handle_user_answer(event, user_answer):
+    user_id = event.source.user_id
+    question_title = redis_client.hget(user_id, "current_question")
+
+    if question_title:
+        # 存儲用戶的答案
+        redis_client.hset(user_id, "user_answer", user_answer)
+
+        # 從 Redis 中讀取學號
+        student_id = redis_client.hget(user_id, "student_id")
+
+        if student_id:
+            # 確保題目和答案的鍵存在於 Redis 中
+            qa_key = f"{student_id}_qa:{question_title}"
+
+            # 將用戶的答案存儲在列表中
+            redis_client.rpush(qa_key, user_answer)
+        else:
+            # 如果無法找到學號，則拋出錯誤或採取其他適當的處理方式
+            print("無法找到學號，無法存儲題目和用戶答案")
+
+        # 將題目發送給 Llama3 伺服器並回傳答案
+        llama3_answer = send_question_to_llama3(question_title)
+
+        # 組合要回覆的文字訊息，包括用戶的回答和 Llama3 的回答
+        reply_text = f"題目：{question_title}\n\n您回答：{user_answer}\n\nLlama3 回答：{llama3_answer}"
+
+        # 使用 TextSendMessage 回覆文字訊息
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+
+        # 清除當前題目和用戶回答
+        redis_client.hdel(user_id, "current_question")
+        redis_client.hdel(user_id, "user_answer")
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="未找到對應的問題，請重新選擇題目。"))
+
+def handle_suggestion(event):
+    user_id = event.source.user_id
+    suggestion = event.message.text
+
+    if suggestion:
+        suggestion_data = {
+            "user_id": user_id,
+            "suggestion": suggestion
+        }
+        suggestion_collection.insert_one(suggestion_data)
+        redis_client.hdel(user_id, 'awaiting_suggestion')
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="謝謝您的建議！我們會努力改進。"))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請輸入您的建議。"))
+
 def send_question_to_llama3(question):
     llama3_server_url = 'http://192.168.100.137:5000/ask'
+
     try:
         print(f"Sending question to Llama3 server: {llama3_server_url}")
         response = requests.post(llama3_server_url, json={'question': question})
@@ -228,8 +226,6 @@ def send_question_to_llama3(question):
         print(f"Error sending question to Llama3: {e}")
         return '無法獲取回答'
 
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
@@ -237,24 +233,31 @@ def handle_message(event):
     user_name = user_profile.display_name
 
     msg = event.message.text
+    awaiting_suggestion = redis_client.hget(user_id, 'awaiting_suggestion')
 
-    if '學號' in msg or is_valid_student_id(msg):
-        reply = handle_student_id(user_id, user_name, msg)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
-    elif msg == "我要作答":
-        handle_unit_selection(event)
-    elif msg in unit_collections:
-        handle_question_display(event, msg)
-    elif msg.startswith("回答"):
-        question_title = msg[2:]
-        handle_question_answer(event, question_title)
+    if awaiting_suggestion:
+        handle_suggestion(event)
+    elif redis_client.hexists(user_id, 'student_id') and mongo_collection.find_one({"user_id": user_id}):
+        if msg == "我要作答":
+            handle_unit_selection(event)
+        elif msg == "歡迎留下您寶貴的建議:D":
+            redis_client.hset(user_id, 'awaiting_suggestion', 'true')
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請輸入您的建議。"))
+        elif msg in unit_collections:
+            handle_question_display(event, msg)
+        elif msg.startswith("回答"):
+            question_title = msg[2:]
+            handle_question_answer(event, question_title)
+        elif redis_client.hget(user_id, "current_question"):
+            handle_user_answer(event, msg)
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="無效指令，請重新輸入。"))
     else:
-        # 這裡新增了將用戶問題發送給 Llama3 伺服器的部分
-        llama3_answer = send_question_to_llama3(msg)
-        reply = handle_question_reply(user_id, user_name, msg)
-        full_reply = f"{reply}\nLlama3 回答: {llama3_answer}"
-        print(f"Sending reply to user: {full_reply}")
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=full_reply))
+        if is_valid_student_id(msg):
+            reply = handle_student_id(user_id, user_name, msg)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請先輸入符合格式的學號（8位數字）。"))
 
 @app.route("/", methods=['POST'])
 def callback():
